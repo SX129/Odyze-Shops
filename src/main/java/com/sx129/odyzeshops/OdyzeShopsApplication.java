8d376d9858d4554e2148bd9f55192a4dd9bb9463
Henry Nguyen <nguyenhenry682@gmail.com>
2024-10-01 20:39:44 -0500

    initial commit

---
+++ src/main/java/com/sx129/odyzeshops/OdyzeShopsApplication.java
@@ -0,0 +1,13 @@
+package com.sx129.odyzeshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdyzeShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdyzeShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sx129/odyzeshops/OdyzeShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sx129.odyzeshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdyzeShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
