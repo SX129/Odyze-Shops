a6dd01f71291421ac538fd1e33a4e549da490a23
Henry Nguyen <nguyenhenry682@gmail.com>
2024-10-07 13:09:13 -0500

    implemented cartitem service methods

---
+++ src/main/java/com/sx129/odyzeshops/service/cart/CartItemService.java
@@ -0,0 +1,84 @@
+package com.sx129.odyzeshops.service.cart;
+
+import com.sx129.odyzeshops.exceptions.ResourceNotFoundException;
+import com.sx129.odyzeshops.model.Cart;
+import com.sx129.odyzeshops.model.CartItem;
+import com.sx129.odyzeshops.model.Product;
+import com.sx129.odyzeshops.repository.CartItemRepository;
+import com.sx129.odyzeshops.repository.CartRepository;
+import com.sx129.odyzeshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService{
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+
+        CartItem cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(new CartItem());
+
+        if (cartItem.getId() == null) {
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setCart(cart);
+            cartItem.setUnitPrice(product.getPrice());
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setTotalPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId){
+        Cart cart = cartService.getCart(cartId);
+        return cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Item not found in cart"));
+    }
+}
---
+++ src/main/java/com/sx129/odyzeshops/service/cart/ICartItemService.java
@@ -0,0 +1,11 @@
+package com.sx129.odyzeshops.service.cart;
+
+import com.sx129.odyzeshops.model.CartItem;
+
+public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
+}
