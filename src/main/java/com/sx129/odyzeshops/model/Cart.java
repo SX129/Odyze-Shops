5c2774e22ca932d84eda6d21a0606d18aa41f86c
Henry Nguyen <nguyenhenry682@gmail.com>
2024-10-07 13:01:00 -0500

    updated cart model with methods

--- src/main/java/com/sx129/odyzeshops/model/Cart.java
+++ src/main/java/com/sx129/odyzeshops/model/Cart.java
@@ -23,4 +23,26 @@ public class Cart {
     //Relationships
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> cartItems;
+
+    public void addItem(CartItem item) {
+        this.cartItems.add(item);
+        item.setCart(this);
+        updateTotalAmount();
+    }
+
+    public void removeItem(CartItem item) {
+        this.cartItems.remove(item);
+        item.setCart(null);
+        updateTotalAmount();
+    }
+
+    private void updateTotalAmount() {
+        this.totalAmount = cartItems.stream().map(item -> {
+            BigDecimal unitPrice = item.getUnitPrice();
+            if (unitPrice == null) {
+                return  BigDecimal.ZERO;
+            }
+            return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
+        }).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
