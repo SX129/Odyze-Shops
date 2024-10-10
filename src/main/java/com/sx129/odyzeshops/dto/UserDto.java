c5e713611fc3091a42fdcd33ceeba2c7061f6084
Henry Nguyen <nguyenhenry682@gmail.com>
2024-10-09 23:28:18 -0500

    created dto for user, cart, and cartitem models

---
+++ src/main/java/com/sx129/odyzeshops/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.sx129.odyzeshops.dto;
+
+import com.sx129.odyzeshops.model.Cart;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
+}
